b0b2cbf8c2b0ad5cbb7105cb006aacd20ee392f7
Hadley Edwards <hadley.edwards@cynatglobal.com>
2025-07-18 16:39:26 -0400

    Configure logging for progress utilities

--- src/flet_datascope_test.py
+++ src/flet_datascope_test.py
@@ -10,6 +10,7 @@ import json
 import sys
 
 # Set up module level logger for debug output
+logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 # Global variable to store the DF (SEAN FEATURE BUILDOUT)
